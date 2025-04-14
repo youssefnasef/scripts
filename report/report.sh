c0eb961b472b50c0ec169068b3df18a01ebba9fc
youssefnasef <104765453+youssefnasef@users.noreply.github.com>
2025-04-14 16:03:46 +0200

    Update report.sh

--- report/report.sh
+++ report/report.sh
@@ -5,7 +5,7 @@ mkdir -p /home/youssef/Documents/reports/tree
 
 #######################
 tree -h --du /mnt/2tb_ar/Mediaa > /home/youssef/Documents/reports/tree/ar.txt
-tree -h --du /mnt/2tb_en/mediaa2 > /home/youssef/Documents/reports/tree/en.txt
+tree -h --du /mnt/4tb_en/mediaa2 > /home/youssef/Documents/reports/tree/en.txt
 tree -h --du /mnt/2tb_mix/media > /home/youssef/Documents/reports/tree/mix.txt
 
 
@@ -14,20 +14,20 @@ ls /mnt/2tb_ar/Mediaa/2\ -\ مسلسلات > /home/youssef/Documents/reports/ls/
 tree -h --du /mnt/2tb_ar/Mediaa/2\ -\ مسلسلات > /home/youssef/Documents/reports/tree/tv-ar.txt
 
 #####################
-ls /mnt/2tb_ar/Mediaa/1\ -\ افلام > /home/youssef/Documents/reports/ls/movies-ar.txt
-tree -h --du /mnt/2tb_ar/Mediaa/1\ -\ افلام > /home/youssef/Documents/reports/tree/movies-ar.txt
+ls /mnt/4tb_en/mediaa2/1\ -\ افلام > /home/youssef/Documents/reports/ls/movies-ar.txt
+tree -h --du /mnt/4tb_en/mediaa2/1\ -\ افلام > /home/youssef/Documents/reports/tree/movies-ar.txt
 
 ##################
 ls /mnt/2tb_ar/Mediaa/3\ -\ مسرحيات > /home/youssef/Documents/reports/ls/tv-m-ar.txt
 tree -h --du /mnt/2tb_ar/Mediaa/3\ -\ مسرحيات > /home/youssef/Documents/reports/tree/tv-m-ar.txt
 
 ####################
-ls /mnt/2tb_en/mediaa2/6\ -\ tv\ show > /home/youssef/Documents/reports/ls/tv-en.txt
-tree -h --du /mnt/2tb_en/mediaa2/6\ -\ tv\ show > /home/youssef/Documents/reports/tree/tv-en.txt
+ls /mnt/4tb_en/mediaa2/6\ -\ tv\ show > /home/youssef/Documents/reports/ls/tv-en.txt
+tree -h --du /mnt/4tb_en/mediaa2/6\ -\ tv\ show > /home/youssef/Documents/reports/tree/tv-en.txt
 
 #########################
-ls /mnt/2tb_en/mediaa2/8\ -\ movies > /home/youssef/Documents/reports/ls/movies-en.txt
-tree -h --du /mnt/2tb_en/mediaa2/8\ -\ movies > /home/youssef/Documents/reports/tree/movies-en.txt
+ls /mnt/4tb_en/mediaa2/8\ -\ movies > /home/youssef/Documents/reports/ls/movies-en.txt
+tree -h --du /mnt/4tb_en/mediaa2/8\ -\ movies > /home/youssef/Documents/reports/tree/movies-en.txt
 
 ######################
 ls /mnt/2tb_mix/media/4\ -\ anime > /home/youssef/Documents/reports/ls/anime.txt
